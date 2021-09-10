f901a4fc00a15d9f05b33552ef179ee5c18a3f49
Fyllio <90462270+Fyllio@users.noreply.github.com>
2021-09-10 17:30:54 +0200

    Uploaded cs code to repo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static bool running = true;
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(5000);
+            while (running)
+            {
+                foreach (Process p in Process.GetProcesses())
+                {
+
+                    if (p.ProcessName == "javaw")
+                    {
+                        ShowWindow(p.MainWindowHandle, 2);
+                        running = false;
+                    }
+                }
+            }
+        }
+    }
+}
